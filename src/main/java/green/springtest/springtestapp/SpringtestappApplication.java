94ae4968aa0f9755469fa726594188a6b2b7ee8b
GreenThaMan <tengisluigi@yahoo.com>
2021-03-19 21:54:13 +0800

    Initialize spring boot project
    spring.io initializr

---
+++ src/main/java/green/springtest/springtestapp/SpringtestappApplication.java
@@ -0,0 +1,13 @@
+package green.springtest.springtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/green/springtest/springtestapp/SpringtestappApplicationTests.java
@@ -0,0 +1,13 @@
+package green.springtest.springtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
